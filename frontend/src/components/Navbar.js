b08f5da847e345904ce8c92c964f077dd0649741
Jason Pennini <jason.pennini@gmail.com>
2024-10-30 20:53:51 -0700

    added logo to navbar and adjusted navbar formatting

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,25 @@
-const Navbar = () => {
+import React from 'react';
+import '../index.css';
 
+const Navbar = () => {
   return (
-    <header>
-      <div className='container'>
-        <h1> Batted Ball Scatterplots</h1>
+    <header className="bg-white py-3">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col">
+            <h1 className="m-0">Batted Ball Scatterplots</h1>
+          </div>
+          <div className="col-auto">
+            <img 
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Atlanta_Braves_Insignia.svg/2188px-Atlanta_Braves_Insignia.svg.png" 
+              alt="Atlanta Braves Logo" 
+              className="logo"
+            />
+          </div>
+        </div>
       </div>
     </header>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
